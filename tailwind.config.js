030dddb8cdda951d1e2f98e0e9d48df026334e99
Nhud4 <nu12ulhud4@gmail.com>
2023-08-14 11:42:59 +0700

    delete template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      'white': '#FFFFFF',
-      'primary': {
-        '10': '#237B9F',
-        '20': '#51ABC5',
-        '30': '#BEC8FF'
-      },
-      'natural': {
-        '10': '#3DDE43',
-        '20': '#3DDE43',
-        '30': '#F1FEE8'
-      },
-      'danger':{
-        '10': '#FF7555',
-        '20': '#FF4135',
-        '30': '#FFF2E7'
-      },
-      'cyn':{
-        '10': '#FF9F26'
-      },
-      'nero':{
-        '10': '#EFF4F0',
-        '20': '#D2D4DA'
-      }
-    },
     extend: {
-      fontSize: {
-        '10': '10px',
-        '11': '11px',
-        '13': '13px',
-        '14': '14px',
-        '16': '16px',
-        '20': '20px',
-        '22': '22px',
-        '35': '35px'
-      },
-      boxShadow: {
-        '1': '0px 8px 24px rgba(0, 0, 0, 0.1)',
-        '2': '0px 4px 10px 0px rgba(0, 0, 0, 0.1)'
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
+}
