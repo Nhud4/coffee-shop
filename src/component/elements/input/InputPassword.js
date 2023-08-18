90c2efa7a5ffb3768c290b53b091bfa0eb212301
Nhud4 <nu12ulhud4@gmail.com>
2023-08-18 20:22:41 +0700

    fix(input):fix capitalize

--- src/component/elements/input/InputPassword.js
+++ src/component/elements/input/InputPassword.js
@@ -11,7 +11,7 @@ export default function InputPassword({ label, onChange, value }){
     inactiveBox: 'flex items-center p-2 rounded border-2 border-cyn-20 h-[2.5rem]',
     label: 'text-base font-medium text-black-500 pb-2',
     inputBox: 'w-full text-base text-black-500 bg-transparent outline-none border-none',
-    inputActive: 'w-full text-base text-white bg-cyn-20  outline-none border-none capitalize'
+    inputActive: 'w-full text-base text-white bg-cyn-20  outline-none border-none'
   };
 
   return (
